886c68ba06f14d7b500278beb34c244e9a28a914
Jahnavi Vemuri <jvemuri@umd.edu>
2024-12-15 17:43:31 -0500

    Editied comment

--- fixedDice.py
+++ fixedDice.py
@@ -1,4 +1,4 @@
-# Determines if any two values rolled are equal to eachother
+# Determines if any two values rolled are equal to eachother or else it will just return the set if its not a fixed roll
 
 def fixed_dice(roll):
     if roll[0] == roll[1]:
