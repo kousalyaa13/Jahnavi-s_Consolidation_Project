3c7390732ac504e90f73e7a21e3cad0c857d0423
Jahnavi Vemuri <jvemuri@umd.edu>
2024-12-15 17:43:43 -0500

    Specified comment

--- tupleOut.py
+++ tupleOut.py
@@ -1,4 +1,4 @@
-# Determines if the rolls are considered tupled out
+# Determines if the rolls are considered tupled out by seeing if each if the values rolled are all equal to eachother
 
 def tuple_out(roll):
     return roll[0] == roll[1] == roll[2]
